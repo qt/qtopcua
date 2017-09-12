4c8e746dca0afdcae5e0564a9d216c73f43f6e84
Alex Blasche <alexander.blasche@qt.io>
2017-09-12 10:23:44 +0200

    Suppress Wunused-parameter warning from open62541.h
    This change solves the problem (in a more generic way) for all
    compilers.
    
    Change-Id: I3236717d6269599e4a1f1e18a7ec436e1848f46b
    Reviewed-by: Maurice Kalinowski <maurice.kalinowski@qt.io>

--- src/plugins/opcua/open62541/qopen62541.h
+++ src/plugins/opcua/open62541/qopen62541.h
@@ -38,15 +38,25 @@
 #define QOPEN62541_H
 #include <QtCore/QtGlobal>
 
-#ifdef Q_OS_WIN
+#if defined(_MSC_VER)
 #pragma warning(push)
 #pragma warning(disable: 4100) // unused variable
+#elif defined(__GNUC__) //includes mingw
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#elif defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
 #endif
 
 #include <open62541.h>
 
-#ifdef Q_OS_WIN
+#if defined(_MSC_VER)
 #pragma warning(pop)
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#elif defined(__clang__)
+#pragma clang diagnostic pop
 #endif
 
 #endif // QOPEN62541_H
