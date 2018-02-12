c5cd586c8c20a381e2f54e91693e794f03a651fb
Jannis Voelker <jannis.voelker@basyskom.com>
2018-02-12 16:16:44 +0100

    Add the "we mean it" warning to qopcuabinarydataencoding_p.h
    Change-Id: I47a8b78a14301cd9c27705b50e3c1e2f0aa6feca
    Reviewed-by: Frank Meerkoetter <frank.meerkoetter@basyskom.com>
    Reviewed-by: Maurice Kalinowski <maurice.kalinowski@qt.io>

--- src/opcua/client/qopcuabinarydataencoding_p.h
+++ src/opcua/client/qopcuabinarydataencoding_p.h
@@ -37,6 +37,17 @@
 #ifndef QOPCUABINARYDATAENCODING_H
 #define QOPCUABINARYDATAENCODING_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include "qopcuaglobal.h"
 #include "qopcuatype.h"
 
