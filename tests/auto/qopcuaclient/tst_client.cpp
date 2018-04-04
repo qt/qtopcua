2882b8cc585e6b02adfc1f351c1f1bf066011667
Jannis Voelker <jannis.voelker@basyskom.com>
2018-04-04 15:24:49 +0200

    Disable the freeopcua tests when running in the CI
    The freeopcua tests randomly run into timeouts when there is high load
    on the CI. This makes the integration of changes almost impossible.
    
    As the previous solution to the timeouts stopped working a few weeks
    ago and no new solution has been found, the freeopcua tests are disabled
    for now.
    
    Change-Id: I9d16fa55d8c6b45c017de10cee3facd643c76dd5
    Task-number: QTBUG-66704
    Reviewed-by: Maurice Kalinowski <maurice.kalinowski@qt.io>
    Reviewed-by: Frank Meerkoetter <frank.meerkoetter@basyskom.com>

--- tests/auto/qopcuaclient/tst_client.cpp
+++ tests/auto/qopcuaclient/tst_client.cpp
@@ -315,6 +315,12 @@ Tst_QOpcUaClient::Tst_QOpcUaClient()
 void Tst_QOpcUaClient::initTestCase()
 {
     for (const auto &backend: m_backends) {
+        // QT_TEST_CI is set to 1 in the Qt CI, see https://wiki.qt.io/CI_Environment
+        if (backend == QStringLiteral("freeopcua") && qgetenv("QT_TEST_CI") == QStringLiteral("1")) {
+            qDebug() << "The freeopcua tests randomly fail in the CI and are disabled by default";
+            continue;
+        }
+
         QOpcUaClient *client = m_opcUa.createClient(backend);
         QVERIFY2(client != nullptr,
                  QString("Loading backend failed: %1").arg(backend).toLatin1().data());
