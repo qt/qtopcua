3cfd1f282258477d875482cf9df0dcd3de3019e1
Maurice Kalinowski <maurice.kalinowski@qt.io>
2020-03-14 22:05:35 +0100

    Revert "Prospective build fix for MinGW"
    This reverts commit 58a4d5b54968bdcd042113a1a5200db7e6f7547b.
    
    The design changes for MinGW caused some regressions, which needed
    to be reverted. The same applies for this patch.
    
    Task-number: QTBUG-82868
    Change-Id: I11ede47795e019037927fe5a0966e791ba5fe82f
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

--- tests/auto/clientSetupInCpp/tst_clientSetupInCpp.cpp
+++ tests/auto/clientSetupInCpp/tst_clientSetupInCpp.cpp
@@ -124,7 +124,7 @@ public slots:
                                          + QLatin1String("/../../open62541-testserver/open62541-testserver.app/Contents/MacOS/open62541-testserver")
 #else
 
-#if defined(Q_OS_WIN) && QT_CONFIG(debug_and_release)
+#ifdef Q_OS_WIN
                                          + QLatin1String("/..")
 #endif
                                          + QLatin1String("/../../open62541-testserver/open62541-testserver")
