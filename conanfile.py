f355757e47b1621ba495d5b625980d84e0ddddd8
Iikka Eklund <iikka.eklund@qt.io>
2021-02-04 08:16:04 +0200

    Conan: Use the 'shared' option value of the Qt build
    Default to the value of the prebuilt Qt build.
    
    The 'default' needs to be added as valid option in the list which
    will be used by default if no value is given by the user or via
    the conan profile.
    
    The user is still able to override this via the profile or from
    command line if needed:
    
      "conan install ... -o shared=False"
    
    CMakeCache.txt:
    
      - "conan install ..."
        - BUILD_SHARED_LIBS:BOOL=ON
      - "conan install ... -o shared=True"
        - BUILD_SHARED_LIBS:BOOL=ON
      - "conan install ... -o shared=False"
        - BUILD_SHARED_LIBS:BOOL=OFF
    
    Task-number: QTBUG-90901
    Change-Id: Ic71ac82b8b4bda0ae508680094bafc2d6c00ea13
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit 4acff10f8582d8384a2875daa771b2777788995a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- conanfile.py
+++ conanfile.py
@@ -43,9 +43,9 @@ class QtOPCUA(ConanFile):
     description = "The Qt OPC UA module implements a Qt API to interact with OPC UA on top of a 3rd party OPC UA stack."
     topics = ("qt", "qt6", "OPC UA", "OPCUA")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False],
+    options = {"shared": [True, False, "default"],
                "qt6": "ANY"}  # this is needed to model unique package_id for the Add-on build per used Qt6 version
-    default_options = {"shared": False,
+    default_options = {"shared": "default",  # default: Use the value of the Qt build
                        "qt6": None}
     exports_sources = "*", "!conan*.*"
 
@@ -86,6 +86,10 @@ class QtOPCUA(ConanFile):
         # Note, the actual 'make install' is called in "package()".
         install_dir = os.path.join(os.getcwd(), "_install_tmp")
         cmake.definitions["CMAKE_INSTALL_PREFIX"] = install_dir
+
+        # Use the value of the Qt build
+        if self.options.shared.value == "default":
+            del cmake.definitions["BUILD_SHARED_LIBS"]
 
         cmake_toolchain_file = os.environ.get("CMAKE_TOOLCHAIN_FILE")
         if cmake_toolchain_file:
