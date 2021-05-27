a9330ac6b0571e609ed45cf2a760b5c708dce6cc
Kai Köhne <kai.koehne@qt.io>
2021-05-27 10:46:35 +0200

    Fix MSVC warning C4005 in open62541.c
    Upstream merge request: https://github.com/open62541/open62541/pull/4439
    
    Change-Id: I70ebdef6472a559a9374398563e61e2e0d9f4466
    Reviewed-by: Frank Meerkoetter <frank.meerkoetter@basyskom.com>

--- src/3rdparty/open62541/open62541.c
+++ src/3rdparty/open62541/open62541.c
@@ -21,7 +21,7 @@
 #endif
 
 /* Disable security warnings for BSD sockets on MSVC */
-#ifdef _MSC_VER
+#if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
 # define _CRT_SECURE_NO_WARNINGS
 #endif
 
