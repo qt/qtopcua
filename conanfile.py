d990dabe6e5c07af80b7615970458f11a22a62f2
Jani Heikkinen <jani.heikkinen@qt.io>
2021-06-08 07:49:24 +0300

    Bump version
    Change-Id: Ic030255fe6e00717c447a9fa61c70c79db6ef79e

--- conanfile.py
+++ conanfile.py
@@ -37,7 +37,7 @@ class QtConanError(Exception):
 
 class QtOPCUA(ConanFile):
     name = "qtopcua"
-    version = "6.1.1"
+    version = "6.1.2"
     license = "GPL-3.0+, LGPL-3.0+, Commercial Qt License Agreement"
     author = "The Qt Company <https://www.qt.io/contact-us>"
     url = "https://code.qt.io/cgit/qt/qtopcua.git"
