d178d3d11e3e0cf7596e68c1b64061a3215b25cb
Jannis Voelker <jannis.voelker@basyskom.com>
2021-09-24 16:50:41 +0200

    Bump the year in the header written by the namespace 0 ids generator
    Change-Id: Ie8d8a2e82dea166f27b4650e2ed67513149b0737
    Reviewed-by: Frank Meerkoetter <frank.meerkoetter@basyskom.com>

--- tools/defaultnodeidsgenerator/main.cpp
+++ tools/defaultnodeidsgenerator/main.cpp
@@ -59,7 +59,7 @@ void createLicenseHeader(QTextStream &output)
 {
     output << R"(/****************************************************************************
 **
-** Copyright (C) 2018 basysKom GmbH, opensource@basyskom.com
+** Copyright (C) 2021 basysKom GmbH, opensource@basyskom.com
 ** Contact: http://www.qt.io/licensing/
 **
 ** This file is part of the QtOpcUa module of the Qt Toolkit.
