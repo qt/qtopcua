743cb96ad4aa006a8b8d047e8735335713b8617a
Lucie Gérard <lucie.gerard@qt.io>
2022-08-23 10:06:16 +0200

    Change the license of all CMakeLists.txt and *.cmake files to BSD
    Task-number: QTBUG-105718
    Change-Id: Ia8401e033fc4b7414e798d1cc5da90977ad5f29b
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Jörg Bornemann <joerg.bornemann@qt.io>

--- src/opcua/Qt6OpcUaMacros.cmake
+++ src/opcua/Qt6OpcUaMacros.cmake
@@ -1,5 +1,5 @@
 # Copyright (C) 2022 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+# SPDX-License-Identifier: BSD-3-Clause
 
 # Removes optimizations flags in the called directory scope.
 function(qt_opcua_disable_optimizations_in_current_dir)
