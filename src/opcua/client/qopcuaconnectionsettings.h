d6e1e086e42a6b1e5522c60df2cacb2f2fffe316
Marc Mutz <marc.mutz@qt.io>
2023-08-14 11:39:40 +0200

    QOpcUaConnectionSettings: unexport
    Non-polymorphic classes should not be exported wholesale. The fact
    that MSVC exports inline members of such classes, and that debug
    builds call the DLL-exported functions instead of inlining has caused
    a steady trickle of bugs and annoyances.
    
    Only export public out-of-line functions.
    
    Found in API-review.
    
    Change-Id: Ie785cbeca50cd3cbd85ddb6db3ae7e9f8ad24043
    Reviewed-by: Alex Blasche <alexander.blasche@qt.io>
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 375aebc0cfd18a065e2355eddbc09e30591b6768)

--- src/opcua/client/qopcuaconnectionsettings.h
+++ src/opcua/client/qopcuaconnectionsettings.h
@@ -14,28 +14,28 @@
 QT_BEGIN_NAMESPACE
 
 class QOpcUaConnectionSettingsData;
-class Q_OPCUA_EXPORT QOpcUaConnectionSettings
+class QOpcUaConnectionSettings
 {
 public:
-    QOpcUaConnectionSettings();
-    QOpcUaConnectionSettings(const QOpcUaConnectionSettings &other);
-    QOpcUaConnectionSettings &operator=(const QOpcUaConnectionSettings &rhs);
-    ~QOpcUaConnectionSettings();
+    Q_OPCUA_EXPORT QOpcUaConnectionSettings();
+    Q_OPCUA_EXPORT QOpcUaConnectionSettings(const QOpcUaConnectionSettings &other);
+    Q_OPCUA_EXPORT QOpcUaConnectionSettings &operator=(const QOpcUaConnectionSettings &rhs);
+    Q_OPCUA_EXPORT ~QOpcUaConnectionSettings();
 
-    QStringList sessionLocaleIds() const;
-    void setSessionLocaleIds(const QStringList &localeIds);
+    Q_OPCUA_EXPORT QStringList sessionLocaleIds() const;
+    Q_OPCUA_EXPORT void setSessionLocaleIds(const QStringList &localeIds);
 
-    std::chrono::milliseconds secureChannelLifeTime() const;
-    void setSecureChannelLifeTime(std::chrono::milliseconds lifeTime);
+    Q_OPCUA_EXPORT std::chrono::milliseconds secureChannelLifeTime() const;
+    Q_OPCUA_EXPORT void setSecureChannelLifeTime(std::chrono::milliseconds lifeTime);
 
-    std::chrono::milliseconds sessionTimeout() const;
-    void setSessionTimeout(std::chrono::milliseconds timeout);
+    Q_OPCUA_EXPORT std::chrono::milliseconds sessionTimeout() const;
+    Q_OPCUA_EXPORT void setSessionTimeout(std::chrono::milliseconds timeout);
 
-    std::chrono::milliseconds requestTimeout() const;
-    void setRequestTimeout(std::chrono::milliseconds timeout);
+    Q_OPCUA_EXPORT std::chrono::milliseconds requestTimeout() const;
+    Q_OPCUA_EXPORT void setRequestTimeout(std::chrono::milliseconds timeout);
 
-    std::chrono::milliseconds connectTimeout() const;
-    void setConnectTimeout(std::chrono::milliseconds timeout);
+    Q_OPCUA_EXPORT std::chrono::milliseconds connectTimeout() const;
+    Q_OPCUA_EXPORT void setConnectTimeout(std::chrono::milliseconds timeout);
 
 private:
     friend Q_OPCUA_EXPORT bool operator==(const QOpcUaConnectionSettings &lhs,
