b5660b3c365115a9deb9ef17bf7009edba75789c
Jannis Völker <jannis.voelker@basyskom.com>
2025-09-04 09:09:16 +0200

    Mark security critical code using a header as described in QUIP 23
    Task-number: QTBUG-135765
    Pick-to: 6.8
    Change-Id: If8f3a0c202a9b1e4a2d6279c2fdb6a71f67b8f1c
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit 88cf5892f8c0e35c9f814b0d6013270a321fc642)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 537a9fa751aa128be10011dd1b6072b324b2fd03)

--- src/plugins/opcua/open62541/qopen62541backend.cpp
+++ src/plugins/opcua/open62541/qopen62541backend.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2017 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:network-protocol
 
 #ifdef USE_SYSTEM_OPEN62541
 #include <open62541/client_config_default.h>
